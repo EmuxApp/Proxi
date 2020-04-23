259e773e91d9d539253d39454b6a3efd29028f7f
James-Livesey <jamesliveg@gmail.com>
2020-04-23 14:13:29 +0100

    Add screen animations for better UX

--- www/screens.js
+++ www/screens.js
@@ -8,12 +8,116 @@
 */
 
 var screens = {
-    switch: function(screen = "home") {
-        $("main:not([id='" + screen + "'])").attr("hidden", "");
-        $("main[id='" + screen + "']").attr("hidden", null);
+    switch: function(to = "home") {
+        $("main[id='" + to + "']").css({
+            "left": "0"
+        });
 
-        $("footer a:not([data-screen='" + screen + "'])").removeClass("selected");
-        $("footer a[data-screen='" + screen + "']").addClass("selected");
+        $("main:not([id='" + to + "'])").attr("hidden", "");
+        $("main[id='" + to + "']").removeAttr("hidden");
+
+        if ($("footer a[data-screen='" + to + "']").length > 0) {
+            $("footer a:not([data-screen='" + to + "'])").removeClass("selected");
+            $("footer a[data-screen='" + to + "']").addClass("selected");
+        }
+    },
+
+    moveBack: function(from, to = "home") {
+        $("main[id='" + from + "']").css({
+            "z-index": "2",
+            "left": "0"
+        });
+
+        $("main[id='" + to + "']").css({
+            "z-index": "1",
+            "left": "0",
+            "filter": "brightness(0.5)"
+        });
+
+        $("main[id='" + from + "']").removeAttr("hidden");
+        $("main[id='" + to + "']").removeAttr("hidden");
+
+        $("main[id='" + from + "']").animate({
+            "left": $(window).width() + "px"
+        }, {
+            duration: 350
+        });
+
+        var currentBrightness = 0.5;
+        
+        $({brightness: currentBrightness}).animate({
+            brightness: currentBrightness + 0.5
+        }, {
+            duration: 350,
+            step: function() {
+                $("main[id='" + to + "']").css({
+                    "filter": "brightness(" + this.brightness + ")"
+                });
+
+                currentBrightness = this.brightness;
+            }
+        });
+
+        if ($("footer a[data-screen='" + to + "']").length > 0) {
+            $("footer a:not([data-screen='" + to + "'])").removeClass("selected");
+            $("footer a[data-screen='" + to + "']").addClass("selected");
+        }
+
+        setTimeout(function() {
+            $("main[id='" + from + "']").attr("hidden", "");
+            $("main[id='" + from + "']").css({
+                "left": "0"
+            });
+        }, 350);
+    },
+
+    moveForward: function(from, to = "home") {
+        $("main[id='" + from + "']").css({
+            "z-index": "1",
+            "left": "0",
+            "filter": "brightness(0.5)"
+        });
+
+        $("main[id='" + to + "']").css({
+            "z-index": "2",
+            "left": $(window).width() + "px"
+        });
+
+        $("main[id='" + from + "']").removeAttr("hidden");
+        $("main[id='" + to + "']").removeAttr("hidden");
+
+        $("main[id='" + to + "']").animate({
+            "left": "0"
+        }, {
+            duration: 350
+        });
+
+        var currentBrightness = 1;
+        
+        $({brightness: currentBrightness}).animate({
+            brightness: currentBrightness - 0.5
+        }, {
+            duration: 350,
+            step: function() {
+                $("main[id='" + from + "']").css({
+                    "filter": "brightness(" + this.brightness + ")"
+                });
+
+                currentBrightness = this.brightness;
+            }
+        });
+
+        if ($("footer a[data-screen='" + to + "']").length > 0) {
+            $("footer a:not([data-screen='" + to + "'])").removeClass("selected");
+            $("footer a[data-screen='" + to + "']").addClass("selected");
+        }
+
+        setTimeout(function() {
+            $("main[id='" + from + "']").attr("hidden", "");
+            $("main[id='" + from + "']").css({
+                "left": "0"
+            });
+        }, 350);
     }
 };
 
