9500477bf350fa166cc38813b521b89dfc40d4b8
James-Livesey <jamesliveg@gmail.com>
2020-04-23 19:20:58 +0100

    Add splash screen whilst icon font loads

---
+++ www/script.js
@@ -0,0 +1,14 @@
+/*
+    Proxi
+ 
+    Copyright (C) Emux Technologies. All Rights Reserved.
+ 
+    https://emux.app
+    Licenced by the Emux Open-Source Licence, which can be found at LICENCE.md.
+*/
+
+$(function() {
+    setTimeout(function() {
+        $("#loader").fadeOut(500);
+    }, 1000);
+});
